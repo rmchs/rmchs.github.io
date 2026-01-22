bd95cfe7ebbddac9d76bb7e954985aeeac897fb6
brickwall2900 <brickwall2900@gmail.com>
2026-01-22 15:43:44 +0800

    add image component

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import ImageViewer from "./ImageViewer.svelte";
+
+    let { src, alt, ...others } = $props();
+    let isOpen = $state(false);
+
+    function onImageClicked() {
+        isOpen = true;
+    }
+
+    function closeTheDamnPopup() {
+        isOpen = false;
+    }
+</script>
+
+<enhanced:img src={src} alt={alt} {...others} onclick={onImageClicked} />
+
+{#if isOpen}
+    <ImageViewer src={src} alt={alt} closePopup={closeTheDamnPopup} {...others} />
+{/if}
